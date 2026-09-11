e428ebb007a8d8c3b6d574650a9fb1a08fbffad2
singhsukhjot8194-commits <singhsukhjot8194@gmail.com>
2026-09-11 16:30:45 +0530

    Update files

--- src/features/interview/services/interview.api.js
+++ src/features/interview/services/interview.api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://ai-interview-backend-f0lh.onrender.com",
   withCredentials: true,
 });
 
